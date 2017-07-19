fb12feb7891c4fb116e8e08757da42a1d68aeaa9
John Duplechain <jduplechain@gwii.com>
2017-07-19 13:50:05 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,6 @@ kubectl create -f deploy.yaml --validate=false
 kubectl apply -f ingress.yaml --validate=false
 
 
-
 gcloud docker -- push us.gcr.io/${PROJECT_NAME}/hello
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 kubectl patch deployment docker-hello-google -p '{"spec":{"template":{"spec":{"containers":[{"name":"docker-hello-google","image":"us.gcr.io/gwii-cloud/hello:'"$CIRCLE_SHA1"'"}]}}}}'
