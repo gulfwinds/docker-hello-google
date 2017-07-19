3002ac802741de67c59ac76b7dd582251232a821
John Duplechain <jduplechain@gwii.com>
2017-07-19 08:24:04 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 #sudo /opt/google-cloud-sdk/bin/gcloud docker push us.gcr.io/${PROJECT_NAME}/hello
-udo /opt/google-cloud-sdk/bin/gcloud --quiet components update kubectl
+/opt/google-cloud-sdk/bin/gcloud --quiet components update kubectl
 gcloud docker -- push us.gcr.io/${PROJECT_NAME}/hello
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 kubectl patch deployment docker-hello-google -p '{"spec":{"template":{"spec":{"containers":[{"name":"docker-hello-google","image":"us.gcr.io/circle-ctl-test/hello:'"$CIRCLE_SHA1"'"}]}}}}'
