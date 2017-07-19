374a1593a27d73f67cd6f6af14fe7edeb9ae2c9f
John Duplechain <jduplechain@gwii.com>
2017-07-19 10:17:00 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,10 @@ sudo chown -R $USER /home/ubuntu/.config
 
 #sudo /opt/google-cloud-sdk/bin/gcloud docker push us.gcr.io/${PROJECT_NAME}/hello
 /opt/google-cloud-sdk/bin/gcloud --quiet components update kubectl
-kubectl create -f deploy.yaml --validate=false
+
+#Only needs to run the first time to ensure the deployment is there.
+#kubectl create -f deploy.yaml --validate=false
+
 gcloud docker -- push us.gcr.io/${PROJECT_NAME}/hello
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 kubectl patch deployment docker-hello-google -p '{"spec":{"template":{"spec":{"containers":[{"name":"docker-hello-google","image":"us.gcr.io/gwii-cloud/hello:'"$CIRCLE_SHA1"'"}]}}}}'
